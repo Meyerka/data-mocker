7016e5f016a53d55311e040feff2db36a74e43ad
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-09-06 12:59:58 +0200

    initial commit

---
+++ src/components/FileCreator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Select fields</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Field name</th>
+          <th>Range</th>
+          <th>Unicity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="number" name="range" id />
+          </td>
+          <td>
+            <input type="number" name="range" id />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div>Number of rows</div>
+    <div>Include header ?</div>
+    <div>Field seperator</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileCreator",
+  props: {},
+};
+</script>
+
+<style>
+</style>
