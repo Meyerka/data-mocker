9b58046166e49dbe008a9d2b9359e93164e2ac30
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-09-08 18:36:09 +0200

    Download csv functionality

--- src/components/FileCreator.vue
+++ src/components/FileCreator.vue
@@ -37,6 +37,7 @@
       </select>
     </div>
     {{records}}
+    <button @click="downloadCsv()">Generate csv file</button>
   </div>
 </template>
 
@@ -57,6 +58,26 @@ export default {
     },
     update(record) {
       this.records = record;
+    },
+    downloadCsv() {
+      const rows = [
+        ["name1", "city1", "some other info"],
+        ["name2", "city2", "more info"],
+      ];
+
+      let csvContent = "data:text/csv;charset=utf-8,";
+
+      rows.forEach(function (rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+      });
+      let encodedUri = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "my_data.csv");
+      document.body.appendChild(link); // Required for FF
+
+      link.click();
     },
   },
 };
