e13c32499e244569c3d683215c2fd216a6b3d71a
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-09-14 18:41:23 +0200

    Vuetify UI

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
-    <FileCreator />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark></v-app-bar>
+
+    <v-main>
+      <FileCreator />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import FileCreator from "./components/FileCreator.vue";
+import FileCreator from "./components/FileCreator";
 
 export default {
   name: "App",
+
   components: {
     FileCreator,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-</style>
