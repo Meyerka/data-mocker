86455c7e239a113b9c0141c6a22595897709a86f
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-09-19 18:54:00 +0200

    Created DataSeed component

---
+++ src/components/FileComponents/DataSeed.vue
@@ -0,0 +1,35 @@
+<template></template>
+
+<script>
+export default {
+  name: "DataSeed",
+
+  methods: {
+    seedData() {
+      this.$refs.child[0].field.name = "Matricule";
+      this.$refs.child[0].field.type = "number";
+      this.$refs.child[0].field.range.type = "range";
+      this.$refs.child[0].field.range.content.from = 0;
+      this.$refs.child[0].field.range.content.to = 1000;
+      this.$refs.child[1].field.name = "Nom";
+      this.$refs.child[1].field.type = "text";
+      this.$refs.child[1].field.range.type = "list";
+      this.$refs.child[1].field.range.content.list = [
+        "Karl",
+        "Mathie",
+        "Jean",
+        "Marc",
+        "Pierre",
+      ];
+      this.$refs.child[2].field.name = "N° badge";
+      this.$refs.child[2].field.type = "number";
+      this.$refs.child[2].field.range.type = "range";
+      this.$refs.child[2].field.range.content.from = 10000;
+      this.$refs.child[2].field.range.content.to = 1000000;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
